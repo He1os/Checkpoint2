1068645da2d52163fda317159f74ece03d3d3677
Helena <helena.osseyran@gmail.com>
2023-01-24 13:27:01 +0100

    Initial commit

---
+++ src/CheckTest.java
@@ -0,0 +1,12 @@
+public class CheckTest {
+    public static void knasigt(int a, int b)
+    {
+        int c = a + b;
+    }
+    public static void main(String[] args) {
+        knasigt();
+        System.out.println("Här skriver jag massa saker som ej e relevant");}
+
+    private static void knasigt() {
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void knasigt(int a, int b){
+        int c= a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("This is the Git checkpint 1.1!");
+
+        System.out.println("Här skriver jag massa saker som ej e relevant");
+    }
+    }

992abcb8aaaeb4ad4bb75109dda1df3fc10f4495
Helena <helena.osseyran@gmail.com>
2023-01-24 13:33:34 +0100

    added int a and set a value for it.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     }
     public static void main(String[] args) {
         System.out.println("This is the Git checkpint 1.1!");
-
+        int a= 1;
+        System.out.println(a);
         System.out.println("Här skriver jag massa saker som ej e relevant");
     }
     }
